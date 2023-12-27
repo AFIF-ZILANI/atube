83d4b8f5bb67733f3cee9a4d9b20adc57f8018e8
AFIF ZILANI <afifzilani4566@gmail.com>
2023-12-27 13:08:47 +0600

    ad: create functionality for upload files on cloudinary

---
+++ src/utils/cloud.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (path) => {
+  try {
+    if (path) {
+      const response = await cloudinary.uploader.upload(path, {
+        resource_type: "auto",
+      });
+
+      console.log("file uploaded on cloudinary successfully!");
+
+      return response;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    fs.unlinkSync(path);
+    return null;
+  }
+};
