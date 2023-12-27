ac0f77b8ad1d2584653bf65acb826db211d7f9c0
AFIF ZILANI <afifzilani4566@gmail.com>
2023-12-27 12:34:22 +0600

    add: utils code and middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+import app from "./app.js";
 import { connectDB } from "./db/dbConfig.js";
 import dotenv from "dotenv";
 
 dotenv.config({
   path: "./env",
 });
+const port = process.env.PORT || 4000;
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+    app.listen(port, () => {
+      console.log(`server is running on port ${port}...`);
+    });
+  })
+  .catch((err) => console.log("Mongo Database connection Failed!", err));
